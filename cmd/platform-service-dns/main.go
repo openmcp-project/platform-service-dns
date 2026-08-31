fe4ccad04c8a14f74ad165d0600ab098154d524c
René Schünemann <rene.schuenemann@sap.com>
2026-08-31 10:41:53 +0200

    chore: add FIPS 140-3 validation (#143)
    * chore: enable fips validation
    
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>
    
    * feat: release v0.2.0
    
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>
    
    ---------
    
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>

--- cmd/platform-service-dns/main.go
+++ cmd/platform-service-dns/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/openmcp-project/platform-service-dns/cmd/platform-service-dns/app"
+
+	"github.com/openmcp-project/controller-utils/pkg/fips"
 )
 
 func main() {
+	fips.Verify(context.Background())
+
 	cmd := app.NewPlatformServiceDNSCommand()
 
 	if err := cmd.Execute(); err != nil {
